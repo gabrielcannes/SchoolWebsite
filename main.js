3286134b83b2bb805a4f8c1abfb2b7e4c2528825
gabrielcannes <gabrielcannes.gc@gmail.com>
2022-08-05 10:20:02 -0300

    faqs section

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+// navbar scroll js 
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window_scroll', window.scrollY > 0)
+})
+
+// show/hide faqs
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        // toggle unbind and bind the faq class to the open class depending on its current state
+        faq.classList.toggle('open'); 
+
+        // change icon
+        const icon = faq.querySelector('.faq_icon i')
+        if (icon.className === 'uil uil-plus') {
+            icon.className = 'uil uil-minus'
+        }
+        else icon.className = 'uil uil-plus'
+    })
 })
